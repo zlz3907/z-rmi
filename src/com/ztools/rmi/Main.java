1ef5e79e4ce5fb7fdb3409774449da21bdf09b15
Bliss Chung <bliss@3zso.com>
2015-08-30 00:06:38 +0800

    添加可执行的jar包，添加主函数用来在命令行下启动服务

--- src/com/ztools/rmi/Main.java
+++ src/com/ztools/rmi/Main.java
@@ -2,6 +2,25 @@ package com.ztools.rmi;
 
 public class Main {
   public static void main(String[] args) {
+    System.out.println("开始了");
+    for (int i = 0; (null != args) && i < args.length; i++)
+      System.out.println("参数d：" + args[i]);
 
+    for (int i = 0; (null != args) && i < args.length;) {
+      String arg = args[i];
+      if ("-s".equals(arg) || "--server".equals(arg)) {
+        // TODO: start services
+        System.out.println("start service...");
+        i++; // shift
+        continue;
+      } else if ("-e".equals(arg) || "--execute".equals(arg)) {
+        // -e <handler_name> [arg1 arg2 ...]
+        System.out.println("execute: ...");
+        break;
+      } else if ("-w".equals(arg) || "--waitexe".equals(arg)) {
+        System.out.println("waiting response: ...");
+        break;
+      }
+    }
   }
 }
